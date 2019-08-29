a2fa4758211dbf3e9d5cde12dbc6cc97f2204522
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-08-29 14:04:26 -0700

    fix app name

--- examples/pyspark-pandas-test.py
+++ examples/pyspark-pandas-test.py
@@ -4,7 +4,7 @@ from pyspark.sql import SQLContext
 import pandas as pd
 
 conf = SparkConf()
-conf.setAppName('pyspark-pandas=test')
+conf.setAppName('pyspark-pandas-test')
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
@@ -13,8 +13,8 @@ data = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
         "area": [8.516, 17.10, 3.286, 9.597, 1.221],
         "population": [200.4, 143.5, 1252, 1357, 52.98] }
 
-df_pd = pd.DataFrame(data, columns=data.keys())
-df = sqlContext.createDataFrame(df_pd)
+pd_df = pd.DataFrame(data, columns=data.keys())
+df = sqlContext.createDataFrame(pd_df)
 df.show()
 
 sc.stop()
